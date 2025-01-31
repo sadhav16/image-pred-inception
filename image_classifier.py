69239ce090d0d7059208a6e7e5a8fb35aa30d58d
SADHAV S <sadhavpooja@gmail.com>
2025-01-31 10:55:38 +0530

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -50,4 +50,5 @@ def predict():
     return jsonify({'prediction': predicted_class, 'confidence': float(np.max(output_data))})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=5000, debug=True)
